294612992faa8b617d4d8bed274f63897068a4d9
Avibot <avibot@avisota.org>
2014-03-15 04:04:23 +0100

    Auto-Update translations from transifex

---
+++ module/languages/de/default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-03-11T16:04:04+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['MCE']['gallery']['0'] = 'Galerie';
+
