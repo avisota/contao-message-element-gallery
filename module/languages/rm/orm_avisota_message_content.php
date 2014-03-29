c51f444d9005c6733fa9be47300818fbee5389b7
Avibot <avibot@avisota.org>
2014-03-29 04:04:15 +0100

    Auto-Update translations from transifex

--- module/languages/rm/orm_avisota_message_content.php
+++ module/languages/rm/orm_avisota_message_content.php
@@ -8,12 +8,11 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-03-11T16:04:06+01:00
+ * last-updated: 2014-03-25T14:18:44+01:00
  */
 
-
-$GLOBALS['TL_LANG']['orm_avisota_message_content']['imageSources']['0'] = 'Datotecas da funtauna';
-$GLOBALS['TL_LANG']['orm_avisota_message_content']['imageSources']['1'] = 'Tscherna ina u pliras datotecas u ordinaturs da la structura d\'ordinaturs. Sche ti tschernas in ordinatur vegnan tut las datotecas lien integradas automaticamain.';
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['multiSRC']['0'] = 'Datotecas da funtauna';
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['multiSRC']['1'] = 'Tscherna ina u pliras datotecas u ordinaturs da la structura d\'ordinaturs. Sche ti tschernas in ordinatur vegnan tut las datotecas lien integradas automaticamain.';
 $GLOBALS['TL_LANG']['orm_avisota_message_content']['perRow']['0']   = 'Miniaturas per lingia';
 $GLOBALS['TL_LANG']['orm_avisota_message_content']['perRow']['1']   = 'Il dumber da miniaturas da maletgs per lingia.';
 $GLOBALS['TL_LANG']['orm_avisota_message_content']['sortBy']['0']   = 'Zavrar tenor';
