dfa7dacdd8e419f188fbc34f32fda60d62aa5e80
Sven Baumann <baumann.sv@gmail.com>
2016-02-01 12:53:10 +0100

    register subscribers by event subscribers configuration file

--- contao/config/event_subscribers.php
+++ contao/config/event_subscribers.php
@@ -8,12 +8,13 @@
  *
  * @copyright  way.vision 2016
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @package    avisota/contao-message-element-gallery
+ * @package    avisota/contao-message-element-article
  * @license    LGPL-3.0+
  * @filesource
  */
 
-/**
- * Message elements
- */
-$GLOBALS['TL_MCE']['images'][] = 'gallery';
+use Avisota\Contao\Message\Element\Gallery\DefaultRenderer;
+
+return array(
+    new DefaultRenderer()
+);
