0ae6d60915612c197de86c1652d0973e0e4aa456
Sven Baumann <baumann.sv@gmail.com>
2017-06-11 18:05:51 +0200

    remove handle order source field, dc-general works here correctly yet

--- contao/config/event_subscribers.php
+++ contao/config/event_subscribers.php
@@ -2,21 +2,19 @@
 
 /**
  * Avisota newsletter and mailing system
- * Copyright © 2016 Sven Baumann
+ * Copyright © 2017 Sven Baumann
  *
  * PHP version 5
  *
- * @copyright  way.vision 2016
+ * @copyright  way.vision 2017
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @package    avisota/contao-message-element-article
  * @license    LGPL-3.0+
  * @filesource
  */
 
-use Avisota\Contao\Message\Element\Gallery\DataContainer\OrderSRCField;
 use Avisota\Contao\Message\Element\Gallery\DefaultRenderer;
 
 return array(
-    new DefaultRenderer(),
-    new OrderSRCField()
+    new DefaultRenderer()
 );
